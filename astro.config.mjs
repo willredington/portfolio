5572a2165d7acb79da44416b75c51113d317575a
willredington <will86325@gmail.com>
2026-08-31 16:19:01 -0400

    updated package versions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,16 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://portfolio.willredington.com",
-  integrations: [
-    tailwind({ applyBaseStyles: false }),
-    mdx(),
-    sitemap(),
-  ],
+  integrations: [mdx(), sitemap()],
   output: "static",
   build: {
     assets: "_assets",
+  },
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
